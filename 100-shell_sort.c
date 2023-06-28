43493e6074db98d97c304d3f318aa8cd2493a8e6
Reem99-sina <reemsina2@gmail.com>
2023-06-28 10:40:12 -0700

    shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -27,9 +27,9 @@ void shell_sort(int *array, size_t size)
 size_t gap, i, j;
 	if (array == NULL || size < 2)
 		return;
-	for (gap = 1; gap < (size);)
-		gap = gap * 3 + 1;
-	for (; gap >= 1; gap--)
+	for (gap = size / 2; gap < size;)
+		gap = gap - 1;
+	for (; gap >= 1; gap -= 1)
 	{
 		for (i = gap; i < size; i++)
 		{
@@ -40,6 +40,6 @@ size_t gap, i, j;
 				j -= gap;
 			}
 		}
-		print_array(array, size);
 	}
+	print_array(array, size);
 }
